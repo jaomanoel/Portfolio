d984e77e8e7a9d7d9c3ccd44eac0ec1c8625d812
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-03-12 16:08:14 -0300

    Added hover to footer

--- src/ui/components/surface/Footer/Footer.jsx
+++ src/ui/components/surface/Footer/Footer.jsx
@@ -8,23 +8,39 @@ import Bell from "../../../../assets/icons/footer/bell.svg";
 
 const Footer = () => {
     return (
-        <footer className="w-full flex justify-between items-center px-25 py-[.6rem] bg-bg-dark-03">
-            <div className="flex items-center gap-4 text-white text-sm">
+        <footer className="w-full flex justify-between items-center px-18 bg-bg-dark-03">
+            <a
+                href="#"
+                className="flex items-center gap-4 text-white text-sm hover:bg-bg-dark-01 px-8 h-30"
+            >
                 <img src={Close} alt="close" />
                 <span>0</span>
                 <img src={Danger} alt="danger" />
                 <span>0</span>
-            </div>
+            </a>
 
             <div className="flex items-center gap-8">
-                <div className="flex items-center gap-3">
+                <a
+                    href="#"
+                    className="flex items-center hover:bg-bg-dark-01 gap-3 px-8 h-30"
+                >
                     <img src={Live} alt="Live" />
                     <span className="text-white text-sm">Go Live</span>
-                </div>
+                </a>
 
-                <img src={Feedback} alt="feedback" />
+                <a
+                    href="#"
+                    className="flex items-center hover:bg-bg-dark-01 px-8 h-30"
+                >
+                    <img src={Feedback} alt="feedback" />
+                </a>
 
-                <img src={Bell} alt="bell" />
+                <a
+                    href="#"
+                    className="flex items-center hover:bg-bg-dark-01 px-8 h-30"
+                >
+                    <img src={Bell} alt="bell" />
+                </a>
             </div>
         </footer>
     );
