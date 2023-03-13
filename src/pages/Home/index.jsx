c97956b93de56d80c9c94892d4dbd405daf5a3e2
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-03-12 22:55:15 -0300

    Added link in hire button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,10 +12,10 @@ function Home() {
             <div className="container-view bg-bg mt-40 pb-30 md:pb-0 md:mt-30 h-full 2xl:h-[80vh] relative mx-16 md:mx-0">
                 <div className="centralizador-col-center justify-end">
                     <div className="centralizador-col-center md:flex-row-reverse md:centralizador-row-between justify-end md:justify-between">
-                        <div className="content-img-home md:w-[340px] md:h-[240px] 2xl:w-[633px] 2xl:h-[422px]">
+                        <div className="content-img-home md:w-[540px] md:h-[340px] 2xl:w-[633px] 2xl:h-[422px]">
                             <div className="md:hidden w-full h-full absolute bg-gradient-to-b from-transparent to-black"></div>
                             <img
-                                className="w-full h-full md:w-[340px] md:h-[240px] 2xl:w-[633px] 2xl:h-[422px]"
+                                className="w-full h-full md:w-[540px] md:h-[340px] 2xl:w-[633px] 2xl:h-[422px]"
                                 src="https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
                                 alt=""
                             />
@@ -39,8 +39,9 @@ function Home() {
 
                             <div className="flex flex-col gap-12">
                                 <a
-                                    href="#"
-                                    className="flex items-center justify-center w-full h-48 bg-bg-dark-02 rounded-lg"
+                                    href="https://wa.me/message/3RBRQY4FYXPKB1"
+                                    target={"_blank"}
+                                    className="flex items-center justify-center w-full h-48 bg-bg-dark-02 rounded-lg hover:bg-blue"
                                 >
                                     <p className="text-white text-xl">
                                         Contratar
