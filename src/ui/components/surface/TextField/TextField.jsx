b7882383dd62490f067fd46125b206c30d77052a
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-03-14 16:17:09 -0300

    Added props

--- src/ui/components/surface/TextField/TextField.jsx
+++ src/ui/components/surface/TextField/TextField.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const TextField = ({ Text }) => {
+const TextField = ({ Text, props }) => {
     return (
         <h2
-            className={`text-4xl opacity-5 responsive-textfield font-bold text-white leading-none uppercase`}
+            className={`text-4xl opacity-5 responsive-textfield font-bold text-white leading-none uppercase ${props}`}
         >
             {Text}
         </h2>
